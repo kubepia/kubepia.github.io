e935066bbdd0399104fbb499735d875bc90eb93b
myguddy <seungil.han@gmail.com>
2020-03-06 16:35:41 +0900

    clean config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -54,7 +54,7 @@ module.exports = {
 				{
 					title: "Devops",
 					// path: "/devops/", // optional, which should be a absolute path.
-					collapsable: false, // optional, defaults to true
+					// collapsable: false, // optional, defaults to true
 					sidebarDepth: 1, // optional, defaults to 1
 					children: [
 						{
